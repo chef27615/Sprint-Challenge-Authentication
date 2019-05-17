6689ce6643bafe8c2526a18efdce1b4cf6597496
wei du <weidu@weis-Mac-mini.hsd1.va.comcast.net>
2019-05-17 14:12:05 -0400

    built out front end sign in and sign up, sign up is working

--- frontend_testing/testingfront/src/App.js
+++ frontend_testing/testingfront/src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, NavLink } from 'react-router-dom';
+import SignUp from './signUp/SignUp';
+import SignIn from './signIn/SignIn';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <nav>
+        <NavLink to='/signup'>Sign Up Here</NavLink>
+        &nbsp; | &nbsp;
+        <NavLink to='/signin'>Sign In Here</NavLink>
+        </nav>
       </header>
+      <main>
+        <Route path='/signup' component={SignUp} />
+        <Route path='/signin' component={SignIn} />
+      </main>
     </div>
   );
 }
